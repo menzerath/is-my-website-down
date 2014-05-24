a95fb9ccb2af015366f91c9e24ce4bdeb6e6bcca
Marvin Menzerath <marvin.menzerath@gmx.de>
2014-05-24 20:40:57 +0200

    Bump to v2.1.2-SNAPSHOT

--- src/main/java/eu/menzerath/imwd/Main.java
+++ src/main/java/eu/menzerath/imwd/Main.java
@@ -7,7 +7,7 @@ import java.awt.*;
 public class Main {
     public static final String APPLICATION = "Is My Website Down?";
     public static final String APPLICATION_SHORT = "IMWD";
-    public static final String VERSION = "2.1.1";
+    public static final String VERSION = "2.1.2-SNAPSHOT";
     public static final String URL = "https://github.com/MarvinMenzerath/IsMyWebsiteDown";
     public static final String URL_RELEASE = "https://github.com/MarvinMenzerath/IsMyWebsiteDown/releases";
     public static final int MIN_INTERVAL = 10;
